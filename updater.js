757dea6b168fea8e11b73c23fba50a01692dc53e
HadouKenny <knnyliu@gmail.com>
2013-05-29 21:40:09 +1000

    Create updater.js

---
+++ updater.js
@@ -0,0 +1,10 @@
+function checkIfRemoteFileExists(http://sydneyboyshigh.asia/farq.html)
+{
+    var tmp=new Image;
+    tmp.src=fileToCheck;
+
+    if(tmp.complete)        
+        alert("You are up to date!");        
+    else        
+     alert("An update is available!");        
+}
