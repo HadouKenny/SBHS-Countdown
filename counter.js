c0d4b244a1f4d763f3b7714bd19f5546ef30c5a7
Jerry <jerryyip1@gmail.com>
2013-07-05 15:37:33 +1000

    3.0

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-//counter.js v2.8.1 release
+//counter.js v3.0 release
 var day;
 var todayHours;
 var todayMinutes;
@@ -13,6 +13,13 @@ var changedBells=[];
 var changedHours=[];
 var changedMinutes=[];
 
+var weekNum;
+var weekLetter;
+var fetchSuccess = false;
+
+var i=0;
+var bellRang = false;
+
 setInterval(function doCount(){
     //normal times
     var t1Hours = [9,9,10,10,11,11,12,12,13,14,15];
@@ -26,19 +33,6 @@ setInterval(function doCount(){
     var t3Hours = [9,9,10,10,11,12,13,13,14,14,15];
     var t3Minutes = [25,30,25,30,25,5,0,5,0,20,15];
     var t3Desc = ["School Starts","Period 1","Period 1 Ends","Period 2","Lunch","Period 3","Period 3 Ends","Period 4","Recess","Period 5","Weekend"];
-    /*//assembly times
-    var a1Hours = [9,9,10,10,11,11,12,13,13,13,14,15];
-    var a1Minutes = [0,5,15,20,30,50,10,0,5,55,15,15];
-    var a1Desc = ["School Starts","Period 1","Period 1 Ends","Period 2","Lunch","Lunch 2","Period 3","Period 3 Ends","Period 4","Recess","Period 5","End of Day"];
-
-    var a2Hours = [9,9,10,10,11,11,12,12,13,14,15];
-    var a2Minutes = [0,5,10,15,20,40,30,50,10,10,15];
-    var a2Desc = ["School Starts","Period 1","Transition","Period 2","Recess","Period 3","Lunch","Lunch 2","Period 4","Transition","Period 5","End of Day"];
-
-    var a3Hours = [  9, 9, 10,10,11,12,12,13,13,14,14,15];
-    var a3Minutes = [25,30,35,40,45,5, 25,10,15, 0, 20,15];
-    var a3Desc = ["School Starts","Period 1","Transition","Period 2","Lunch","Lunch 2","Period 3","Transition","Period 4","Recess","Period 5","Weekend"];
-*/
 
     //begin logic
     var now = new Date();
@@ -56,41 +50,57 @@ setInterval(function doCount(){
     <!--BEGIN GRAB SBHS CHANGED TIMES FLAGS, thanks to SBHS IT-->
     if (assemblyChecked==false){
         //var url = 'http://jsonp.jit.su/?callback=?&url=http://student.sbhs.net.au/api/timetable/bells.json?date=2013-'+(now.getMonth()+1)+'-'+now.getDate();
-        var url = 'http://student.sbhs.net.au/api/timetable/bells.json?date=2013-'+(now.getMonth()+1)+'-'+now.getDate()+'&callback=?';
-        //var url = 'http://student.sbhs.net.au/api/timetable/bells.json?date=2013-'+6+'-'+5+'&callback=?';
+        if (nextDay==true){
+            var url = 'http://student.sbhs.net.au/api/timetable/bells.json?date='+(now.getFullYear())+'-'+(now.getMonth()+1)+'-'+(now.getDate()+1)+'&callback=?';
+        }
+        else{
+            var url = 'http://student.sbhs.net.au/api/timetable/bells.json?date='+(now.getFullYear())+'-'+(now.getMonth()+1)+'-'+(now.getDate())+'&callback=?';
+        }
+        /*var url = 'http://student.sbhs.net.au/api/timetable/bells.json?date=2013-6-26&callback=?';*/
         console.log("Getting "+url);
 
-        $.ajax({
-            url: url,
-            dataType: "json",
-            success: function(data){
-                if (data.bellsAltered==true){
-                    assemblyDay = true;
-                    assemblyReason = data.bellsAlteredReason;
-                    console.log("Bell is altered today!");
+        setTimeout(function () {
+            $.ajax({
+                url: url,
+                dataType: "json",
+                success: function(data){
+                    document.getElementById("week").className="animated flipOutX";
 
-                    var temp;
-                    console.log(data.bells[2].time);
-                    for (var p=0; p<data.bells.length; p++){
-                        temp = (data.bells[p].time);
-                        changedBells[p]= temp;
-                    }
-                    console.log(changedBells);
+                    if (data.bellsAltered==true){
+                        assemblyDay = true;
+                        assemblyReason = data.bellsAlteredReason;
+                        console.log("Bell is altered today!");
 
-                    for (var h=0; h<changedBells.length; h++){
-                        changedHours[h]=parseInt(changedBells[h].substr(0,2));
-                        changedMinutes[h]=parseInt(changedBells[h].substr(3,5));
+                        var temp;
+                        for (var p=0; p<data.bells.length; p++){
+                            temp = (data.bells[p].time);
+                            changedBells[p]= temp;
+                        }
+                        console.log(changedBells);
+                        for (var h=0; h<changedBells.length; h++){
+                            changedHours[h]=parseInt(changedBells[h].substr(0,2));
+                            changedMinutes[h]=parseInt(changedBells[h].substr(3,5));
+                        }
                     }
+                    else {
+                        console.log("Bell is not altered. (status:"+data.status+", bellsAltered:"+data.bellsAltered+")");
+                    }
+
+                    weekNum=data.week;
+                    weekLetter=data.weekType;
+
+                    fetchSuccess=true;
+                },
+                error: function(XHR, textStatus, errorThrown){
+                    document.getElementById("week").className="animated fadeOutUp";
+                    console.log("Error Status: " + textStatus);
+                    console.log("Error Thrown: " + errorThrown);
+
+                    document.getElementById("week").innerHTML="<a style='color: #ffbb33'>Real-time data unavailable</a>";
+                    document.getElementById("week").className="animated bounceIn shake"
                 }
-                else {
-                    console.log("Bell is not altered. (status:"+data.status+", bellsAltered:"+data.bellsAltered+")");
-                }
-            },
-            error: function(XHR, textStatus, errorThrown){
-                console.log("Error Status: " + textStatus);
-                console.log("Error Thrown: " + errorThrown);
-            }
-        });
+            });
+        }, 300); /*waste time*/
 
         assemblyChecked=true;
     }
@@ -98,24 +108,6 @@ setInterval(function doCount(){
 
     //grab timetables
     if (assemblyDay==true){
-        /*switch (day){
-            case 3: //wed
-            case 4:
-                todayHours = a2Hours.slice(0);
-                todayMinutes = a2Minutes.slice(0);
-                todayDesc = a2Desc.slice(0);
-                break;
-            case 5: //fri
-                todayHours = a3Hours.slice(0);
-                todayMinutes = a3Minutes.slice(0);
-                todayDesc = a3Desc.slice(0);
-                break;
-            default:
-                todayHours = a1Hours.slice(0);
-                todayMinutes = a1Minutes.slice(0);
-                todayDesc = a1Desc.slice(0);
-                break;
-        }*/
         todayHours = changedHours.slice(0);
         todayMinutes = changedMinutes.slice(0);
         if (day==4 || day==3){
@@ -124,8 +116,8 @@ setInterval(function doCount(){
         else{
             todayDesc = t1Desc.slice(0);
         }
-    }else
-    {
+    }
+    else{
         switch (day){
             case 3: //wed
             case 4:
@@ -144,7 +136,7 @@ setInterval(function doCount(){
                 break;
         }
     }
-    var i = 0;
+    i = 0;
 
     if (nextDay==true){
         todayHours[i] += 24;
@@ -162,7 +154,6 @@ setInterval(function doCount(){
     while ((todayHours[i]*60 + todayMinutes[i] - 1) < nowAbsolute && nowAbsolute < 915){i++}
     if (i==12){i=0}
 
-
     //put last
     var rHours = (todayHours[i]-nowHours);
     var rMinutes = (todayMinutes[i]-nowMinutes-1);
@@ -178,53 +169,82 @@ setInterval(function doCount(){
         rMinutes += 60;
     }
 
-    //display
-    document.getElementById("description").innerHTML= todayDesc[i];
-    if (rHours!=0){
-        document.getElementById("counter").innerHTML= "<b>"+rHours+"</b>h, <b>"+ zeroPad(rMinutes) +"</b>m, <b>"+zeroPad(rSeconds)+"</b>s.";
-    }
-    else{
-        document.getElementById("counter").innerHTML= "<b>"+zeroPad(rMinutes)+"</b>m, <b>"+zeroPad(rSeconds)+"</b>s.";
-    }
 
-    //week
-    if (weekChecked==false && assemblyDay==false){
-        var weekNum = getWeekNumber(now) - 17;
-        var weekLetter;
-        var weekLetterI;
-        var weekLetterArr=["A","B","C"];
-        //weekend correction
-        if ((day==5 && now.getDay()!=4 && (nowHours*60 + nowMinutes)>=915)||day==6||day==0){weekNum+=1}
-        switch (weekNum){
-            case 5:
-            case 8:
-                weekLetterI=0;
-                break;
-            case 6:
-            case 9:
-                weekLetterI=1;
-                break;
-            case 4:
-            case 7:
-                weekLetterI=2;
-                break;
-        }
-        weekLetter=weekLetterArr[weekLetterI];
-        if ((day==5 && now.getDay()!=4 && (nowHours*60 + nowMinutes)>=915)||day==6||day==0){
-
-            document.getElementById("week").innerHTML= "School starts on Week <b>"+weekNum+weekLetter+"</b>";
+    //time display
+    if (assemblyDay==true){
+        document.getElementById("description").innerHTML= todayDesc[i];
+        if (rHours!=0){
+            document.getElementById("counter").innerHTML= "<b><a style='color: #ffbb33'>"+rHours+"</b><a style='color: #ffbb33'></a>h, <b><a style='color: #ffbb33'>"+ zeroPad(rMinutes) +"</a></b>m, <b><a style='color: #ffbb33'>"+zeroPad(rSeconds)+"</a></b>s.";
         }
         else{
-            document.getElementById("week").innerHTML= "Week <b>"+weekNum+weekLetter+"</b>";
+            document.getElementById("counter").innerHTML= "<b><a style='color: #ffbb33'>"+zeroPad(rMinutes)+"</a></b>m, <b><a style='color: #ffbb33'>"+zeroPad(rSeconds)+"</a></b>s.";
+        }
+        document.getElementById("description").className="animated fadeInUp";
+        document.getElementById("counter").className="animated flash";
+
+    }
+    else{
+        document.getElementById("description").innerHTML= todayDesc[i];
+        if (rHours!=0){
+            document.getElementById("counter").innerHTML= "<b>"+rHours+"</b>h, <b>"+ zeroPad(rMinutes) +"</b>m, <b>"+zeroPad(rSeconds)+"</b>s.";
+        }
+        else{
+            document.getElementById("counter").innerHTML= "<b>"+zeroPad(rMinutes)+"</b>m, <b>"+zeroPad(rSeconds)+"</b>s.";
+        }
+        //animation change for next period
+        if (bellRang){  //second run
+            setTimeout(function(){
+                document.getElementById("description").className="animated bounceInLeft waitSmall";
+                document.getElementById("counter").className="animated bounceInLeft";
+            }, 900);
+        }
+        else{ //first run
+            document.getElementById("description").className="animated fadeInUp";
+            document.getElementById("counter").className="animated fadeInDown";
+        }
+    }
+
+    //fun stuff
+    if (rHours==0 && rMinutes==0 && rSeconds==1){
+
+        //document.getElementById("counter", "description").className="";
+        setTimeout(function(){
+            document.getElementById("description").className="animated bounceOutRight waitSmall";
+            document.getElementById("counter").className="animated bounceOutRight";
+        }, 750);
+
+        bellRang=true;
+    }
+
+    //week display
+    if (fetchSuccess && weekChecked==false){
+        if (assemblyDay==false){
+            if ((day==5 && now.getDay()!=4 && (nowHours*60 + nowMinutes)>=915)||day==6||day==0){
+                document.getElementById("week").innerHTML= "School starts on Week <b>"+weekNum+weekLetter+"</b>";}
+            else{ //normal day
+                document.getElementById("week").innerHTML= "Week <b>"+weekNum+weekLetter+"</b>";
+            }
+            document.getElementById("week").className="animated bounceIn";
+        }
+        if (assemblyDay==true && day!=6 && day!=0){
+            document.getElementById("week").innerHTML="<a style='color: #ffbb33'>Changed Belltimes: "+assemblyReason+"</a>";
+            document.getElementById("week").className="animated bounceIn flash";
+        }
+        //show lame jokes on holidays, shamelessly stolen from http://jokes4us.com/miscellaneousjokes/schooljokes/
+        if (weekNum==undefined && weekLetter==undefined){
+            window.location.replace("http://sbhsbelltimes.tk/holiday.html");
         }
         weekChecked=true;
     }
 
-    if (assemblyDay==true && day!=6 && day!=0){
-        document.getElementById("week").innerHTML="Changed Belltimes: "+assemblyReason;
-        weekChecked=true;
-    }
 }, 500);
+
+
+//FAQ
+setInterval(function nag(){
+    document.getElementById("faq").className="";
+    setTimeout(function(){document.getElementById("faq").className="animated wobble";},200);
+}, 5000);
 
 function zeroPad(num) {
     var s = "000" + num;
@@ -239,3 +259,5 @@ function getWeekNumber(d) {
     var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
     return [weekNo];
 }
+
+
